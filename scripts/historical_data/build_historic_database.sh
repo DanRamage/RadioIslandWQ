3d1cb1337e8f438a63ad66cf76822690a996bdff
DANRAMAGE <ChiefDan@gmail.com>
2022-10-12 12:53:44 -0400

    Initial checkin.

--- scripts/historical_data/build_historic_database.sh
+++ scripts/historical_data/build_historic_database.sh
@@ -3,4 +3,4 @@
 source /usr/local/virtualenv/pyenv-3.8.5/bin/activate
 cd /home/xeniaprod/scripts/RadioIslandWQ/scripts/historical_data
 
-python get_observation_data.py --ConfigFile=/home/xeniaprod/scripts/RadioIslandWQ/config/radioisland_historical_build_config_debug.ini --EnteroDataDirectory=/home/xeniaprod/scripts/RadioIslandWQ/data/historical/sampling
+python get_observation_data.py --ConfigFile=/home/xeniaprod/scripts/RadioIslandWQ/config/radioisland_historical_build_config.ini --EnteroDataDirectory=/home/xeniaprod/scripts/RadioIslandWQ/data/historical/sampling
